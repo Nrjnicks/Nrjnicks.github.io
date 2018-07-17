395bc7283392e1245929cbd5022a7ce15ac02f0d
Neeraj S.Thakur <nrjnicks@gmail.com>
2018-07-17 19:56:55 +0800

    Cleaned the js

--- test/resume/js/resumeHeight.js
+++ test/resume/js/resumeHeight.js
@@ -4,30 +4,13 @@ function setSize(){
     document.getElementById("sideNav").style.height= window.outerHeight+"px";
 }
 function resize(){
-    // document.getElementById('resume-iframe').setAttribute("height", sessionStorage.getItem('resumePageSize')); 
     document.getElementById('resume-iframe').setAttribute("height", document.getElementById('resume-iframe').contentWindow.document.body.offsetHeight); 
-}
-function seeking(){
-    
-    console.log(document.getElementById('resume-iframe'));
 }
 function wheel(){
     if(document.getElementById("resume-scroller").classList.contains("active")){
-        // console.log( document.getElementById('resume-iframe').contentWindow.document.getElementById('sideNav').style.top);
-            document.getElementById('resume-iframe').contentWindow.document.getElementById('sideNav').style.top=($(window).scrollTop()-$('iframe').offset().top)+"px";
+        var topPos= ($(window).scrollTop()-$('iframe').offset().top);
+        if(topPos>window.outerHeight) topPos=window.outerHeight;
+        document.getElementById('resume-iframe').contentWindow.document.getElementById('sideNav').style.top=topPos+"px";
 
     }
-    console.log(
-        document.getElementById('resume-iframe').contentWindow.document.getElementById('sideNav').style.top);
-    
 }
-// $( window ).scroll(function() {
-//     if($('li.scroller').hasClass('active')){
-//         $("nav.fixed-top").css({
-//             // sessionStorage.setItem('navTopPadding',document.body.offsetHeight);   
-//         });
-//     }
-    
-    
-//     // console.log(($(window).scrollTop()-$('iframe').offset().top));
-// });
