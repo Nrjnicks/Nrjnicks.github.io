4b2df9e0a15734efe0643bca6e98aef585abbc02
Neeraj Thakur <nrjnicks@gmail.com>
2021-09-27 21:29:47 +0200

    cleaned the JS to be more functional

--- js/projectModal.js
+++ js/projectModal.js
@@ -1,4 +1,6 @@
-var RankedProjects=[
+
+// Global Variables
+var RankedProjects = [
     Projects.Details.WorkInProgress,
     Projects.Details.Homography,
     Projects.Details.ChemicalCarriageway,
@@ -9,121 +11,170 @@ var RankedProjects=[
     Projects.Details.RiTE,
     Projects.Details.NearbyConnections
 ];
-//Functions
-SetPorfolioElements();
-function SetPorfolioElements(){
-    var element = document.getElementById("portfolio-element");
-    var parent = element.parentElement;
-    parent.removeChild(element);//remove template element to start clean
+// Functions
+Start();
+function Start() {
+    SetPorfolioElements();
+    SetKeyboardBinding();
+}
 
-    for (var projectNo = 0; projectNo < RankedProjects.length; projectNo++){
-        var ProjectDetail = RankedProjects[projectNo];
-        // console.log(ProjectDetail.ID);
-        element = element.cloneNode(true);
-        element.firstElementChild.id = ProjectDetail.ID;//change id of first child which is responsible for click
-        element.lastElementChild.firstElementChild.innerHTML= ProjectDetail.Name;
-        var elem = element.getElementsByTagName("img")[0];
-        element.lastElementChild.lastElementChild.innerHTML= ProjectDetail.SmallDescription;
-        elem.setAttribute("src","img/portfolio/"+ProjectDetail.ID+"/thumb.jpg");
-        elem.setAttribute("alt", ProjectDetail.ID+", "+ProjectDetail.Name+", "+ProjectDetail.SmallDescription);
-        elem.setAttribute("title", ProjectDetail.Name+", "+ProjectDetail.SmallDescription);
-        parent.appendChild(element);
-    }
-    $(document).keyup(function(e) {
+// Keybindings
+function SetKeyboardBinding() {
+    $(document).keyup(function (e) {
         if (e.keyCode == 27) { // escape key maps to keycode `27`
             ResetModalParams();
-       }
-   });
+        }
+    });
 }
+
+// Portfolio Section
+function SetPorfolioElements() {
+    var portfolioElement = document.getElementById("portfolio-element");
+    var parent = portfolioElement.parentElement;
+    parent.removeChild(portfolioElement);//remove template element to start clean
+
+    for (var projectNo = 0; projectNo < RankedProjects.length; projectNo++) {
+        var ProjectDetail = RankedProjects[projectNo];
+        // console.log(ProjectDetail.ID);
+        portfolioElement = portfolioElement.cloneNode(true);
+        portfolioElement.firstElementChild.id = ProjectDetail.ID;//change id of first child which is responsible for click
+        portfolioElement.lastElementChild.firstElementChild.innerHTML = ProjectDetail.Name;
+        portfolioElement.lastElementChild.lastElementChild.innerHTML = ProjectDetail.SmallDescription;
+
+        SetPortfolioThumbnail();
+    }
+
+    function SetPortfolioThumbnail() {
+        var imgElement = portfolioElement.getElementsByTagName("img")[0];
+        imgElement.setAttribute("src", `img/portfolio/${ProjectDetail.ID}/thumb.jpg`);
+        imgElement.setAttribute("alt", `${ProjectDetail.ID}, ${ProjectDetail.Name}, ${ProjectDetail.SmallDescription}`);
+        imgElement.setAttribute("title", `${ProjectDetail.Name}, ${ProjectDetail.SmallDescription}`);
+        parent.appendChild(portfolioElement);
+    }
+}
+
+// Show Specific Project
 var currentProjectNumber;
-function ShowPreviousProjectModal(){
-    ResetModalParams();
-    $('#portfolioModal').animate({
-        scrollTop: 0
-    }, 500);
-    ShowProjectModal(RankedProjects[(RankedProjects.length+currentProjectNumber-1)%RankedProjects.length].ID);
-}
-function ShowProjectModal(id){
-    currentProjectNumber=RankedProjects.indexOf(Projects.Details[id]);
+function ShowProjectModal(id) {
+    currentProjectNumber = RankedProjects.indexOf(Projects.Details[id]);
     SetModalParams(id);
+    function SetModalParams(id) {
+        var ProjectDetail = Projects.Details[id];
+        if (ProjectDetail) {
+            ShowLoader();
+            SetProjectDescription();
+            SetProjectDownloadLink();
+            SetCarousel();
+            if (!SetVideoElement()) {
+                HideLoader();
+            }
+        }
+
+        function SetProjectDescription() {
+            document.getElementById("modal-projectname").innerHTML = ProjectDetail.Name;
+            document.getElementById("modal-projectsmalldescription").innerHTML = ProjectDetail.SmallDescription;
+            document.getElementById("modal-projectlargedescription").innerHTML = ProjectDetail.LargeDescription;
+        }
+
+        function SetProjectDownloadLink() {
+            var downloadLinkElement = document.getElementById("modal-downloadlink");
+            if (ProjectDetail.DownloadLink) {
+                ShowElement($(downloadLinkElement));
+                downloadLinkElement.setAttribute('href', ProjectDetail.DownloadLink);
+            }
+            else {
+                HideElement($(downloadLinkElement));
+            }
+        }
+
+        function SetVideoElement() {
+            var videoElement = document.getElementById("modal-videoiframe");
+            if (ProjectDetail.EmbedVideoLinkID) {
+                ShowElement($(videoElement));
+                videoElement.setAttribute('src', `https://www.youtube.com/embed/${ProjectDetail.EmbedVideoLinkID}/?mute=1`);
+                videoElement.setAttribute('onload', HideLoader.name);
+                return true
+            }
+            else {
+                HideElement($(videoElement));
+                return false;
+            }
+        }
+
+        function SetCarousel() {
+            var buttonedIndicatorElmnt = document.getElementById("portfolio-carousel-indicator");
+            var imgContainerElmnt = document.getElementById("portfolio-carousel-inner");
+
+            var buttonedIndicatorElmntParent = buttonedIndicatorElmnt.parentElement;
+            var imgContainerElmntParent = imgContainerElmnt.parentElement;
+
+            buttonedIndicatorElmnt.classList.remove("active");
+            imgContainerElmnt.classList.remove("active");
+
+            DeleteAllChild(buttonedIndicatorElmntParent);
+            DeleteAllChild(imgContainerElmntParent);
+
+            var imgPath = `img/portfolio/${ProjectDetail.ID}/${ProjectDetail.ID}`;
+            for (let i = 1; i <= ProjectDetail.NoOfProjectImages; i++) {
+
+                buttonedIndicatorElmnt.setAttribute("data-slide-to", i);
+                imgContainerElmnt.firstElementChild.setAttribute("src", `${imgPath + i}.jpg`);//as you would guess, string+1=string1
+
+                buttonedIndicatorElmntParent.appendChild(buttonedIndicatorElmnt.cloneNode(true));
+                imgContainerElmntParent.appendChild(imgContainerElmnt.cloneNode(true));
+            }
+            buttonedIndicatorElmntParent.firstElementChild.classList.add("active");
+            imgContainerElmntParent.firstElementChild.classList.add("active");
+
+            function DeleteAllChild(parent) {
+                while (parent.lastElementChild) {
+                    parent.removeChild(parent.lastElementChild)
+                }
+            }
+        }
+    }
 }
-function ShowNextProjectModal(){
+
+
+// Next - Previous Project
+function ShowPreviousProjectModal() {
     ResetModalParams();
-    $('#portfolioModal').animate({
-        scrollTop: 0
-    }, 500);
-    ShowProjectModal(RankedProjects[(currentProjectNumber+1)%RankedProjects.length].ID);
-}
-function ResetModalParams(){
-    document.getElementById("modal-projectname").innerHTML= '';
-    document.getElementById("modal-projectsmalldescription").innerHTML= '';
-    document.getElementById("modal-projectlargedescription").innerHTML= '';
-    document.getElementById("modal-downloadlink").setAttribute('href','');
-    document.getElementById("modal-videoiframe").setAttribute('src','');
-}
-function SetModalParams(id){
-    var ProjectDetail = Projects.Details[id];
-    if(ProjectDetail){ 
-        showLoader();
-        document.getElementById("modal-projectname").innerHTML= ProjectDetail.Name;
-        document.getElementById("modal-projectsmalldescription").innerHTML= ProjectDetail.SmallDescription;
-        document.getElementById("modal-projectlargedescription").innerHTML= ProjectDetail.LargeDescription;
-        var elem= document.getElementById("modal-downloadlink");
-        if(ProjectDetail.DownloadLink){
-            $(elem).show();
-            elem.setAttribute('href', ProjectDetail.DownloadLink);
-        }
-        else{
-            $(elem).hide();
-        }        
-        elem= document.getElementById("modal-videoiframe");
-        if(ProjectDetail.EmbedVideoLinkID){
-            $(elem).show();
-            elem.setAttribute('src', 'https://www.youtube.com/embed/'+ProjectDetail.EmbedVideoLinkID+'/?mute=1');
-            elem.setAttribute('onload',"hideLoader()");
-        }
-        else{
-            $(elem).hide();
-            hideLoader();
-        }
-        SetCarousel(ProjectDetail);        
-    }
-}
-function SetCarousel(ProjectDetail){
-    var indicator = document.getElementById("portfolio-carousel-indicator"); 
-    var inner = document.getElementById("portfolio-carousel-inner"); 
-
-    var indicatorParent = indicator.parentElement;
-    var innerParent = inner.parentElement;
-
-    indicator.classList.remove("active");
-    inner.classList.remove("active");
-
-    DeleteAllChild(indicatorParent);
-    DeleteAllChild(innerParent);
-
-    var imgPath= "img/portfolio/"+ProjectDetail.ID+"/"+ProjectDetail.ID;
-    for (let i = 1; i <= ProjectDetail.NoOfProjectImages; i++) {
-        
-        indicator.setAttribute("data-slide-to",i);
-        inner.firstElementChild.setAttribute("src",imgPath+i+".jpg");
-
-        indicatorParent.appendChild(indicator.cloneNode(true));   
-        innerParent.appendChild(inner.cloneNode(true));     
-    }
-    indicatorParent.firstElementChild.classList.add("active");
-    innerParent.firstElementChild.classList.add("active");
+    ShowProjectModal(RankedProjects[(RankedProjects.length + currentProjectNumber - 1) % RankedProjects.length].ID);
 }
 
-function DeleteAllChild(parent){
-    while(parent.lastElementChild){
-        parent.removeChild(parent.lastElementChild)
+function ShowNextProjectModal() {
+    ResetModalParams();
+    ShowProjectModal(RankedProjects[(currentProjectNumber + 1) % RankedProjects.length].ID);
+}
+
+function ResetModalParams() {
+    document.getElementById("modal-projectname").innerHTML = '';
+    document.getElementById("modal-projectsmalldescription").innerHTML = '';
+    document.getElementById("modal-projectlargedescription").innerHTML = '';
+    document.getElementById("modal-downloadlink").setAttribute('href', '');
+    document.getElementById("modal-videoiframe").setAttribute('src', '');
+    AnimateModalToReset();
+
+    function AnimateModalToReset() {
+        $('#portfolioModal').animate({
+            scrollTop: 0
+        }, 300);
     }
 }
 
-function showLoader(){    
-    $(document.getElementById('loader')).show();
+
+function ShowLoader() {
+    ShowElement($(document.getElementById('loader')));
 }
-function hideLoader(){    
-    $(document.getElementById('loader')).hide();
+
+function HideLoader() {
+    HideElement($(document.getElementById('loader')));
+}
+
+function ShowElement(element) {
+    element.show();
+}
+
+function HideElement(element) {
+    element.hide();
 }
